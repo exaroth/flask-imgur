43929b78137f7470ae7abfd62432cef4e5a76d65
Konrad Wąsowicz <exaroth@gmail.com>
2014-04-10 18:56:37 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Flask-Imgur
+-----------
+
+
+This is simple flask extension allowing uploading
+images straight to Imgur image hosting serveice
+
+
+"""
+
+
+from setuptools import setup
+
+setup(
+    name="Flask-Imgur",
+    version="0.1",
+    url="https://github.com/exaroth/flask-imgur",
+    license="BSD",
+    author="Konrad Wasowicz",
+    author_email="exaroth@gmail.com",
+    description="Upload images straight to Imgur",
+    long_description=__doc__,
+    zip_safe=false,
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Flask", "six"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
